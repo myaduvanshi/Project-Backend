eedf1689644e972f68453fe4e26d522334860494
myaduvanshi <mmukesh1508@gail.com>
2023-12-31 03:59:11 +0530

    modified

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 
 
 //routes import
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.routes.js'
 
 //routes declaration
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({
     path:'./env'
@@ -25,10 +26,11 @@ connectDB()
 
 
 
-import express from "express";
-const app = express()
+
 
 /*
+import express from "express";
+const app = express()
 
 (async ()=>{
     try {
